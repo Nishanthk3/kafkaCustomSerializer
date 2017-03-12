15accb5b8fe5ed5d8e62233346e7c9e183113f21
Nishanthk3 <nishanth.k3@gmail.com>
2017-03-12 16:13:54 -0700

    refactored code

--- src/main/java/com/sample/kafkaCustomSerializer/KafkaConsumerService.java
+++
@@ -1,8 +0,0 @@
-package com.sample.kafkaCustomSerializer;
-
-import java.util.function.Consumer;
-
-public interface KafkaConsumerService {
-
-    public <T> void consume(Consumer<T> callback );
-}
--- src/main/java/com/sample/kafkaCustomSerializer/KafkaProducerService.java
+++
@@ -1,8 +0,0 @@
-package com.sample.kafkaCustomSerializer;
-
-import java.util.concurrent.ExecutionException;
-
-public interface KafkaProducerService {
-    
-    public boolean sendMessage(boolean async, Message message) throws InterruptedException, ExecutionException;
-}
